57dde4e05bbfeaacf06e6b07085b3d35e34651d1
Dangerworld07 <karthikeyan.27csa@licet.ac.in>
2026-04-27 00:13:38 +0530

    Updated project with new changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Welcome To The DangerWorld");
+		System.out.println("Welcome for new developers To The DangerWorld");
 	}
 
 }
